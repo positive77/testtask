4f10ba699351c8733cd5027d40e466255ae2438a
phpuser80 <phpuser80@mail.ru>
2018-04-03 06:43:03 +0300

    change dbname

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=new_db',
+    'dsn' => 'mysql:host=localhost;dbname=testtask_db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
